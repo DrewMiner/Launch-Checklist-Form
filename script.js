7c1b0f0bf3833842df54566d844320063a500059
andwmi <andwmi@gmail.com>
2020-12-16 23:32:23 -0600

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 // Write your JavaScript code here!
+// window.addEventListener("load", function() {
+//    let form = document.querySelector("launchForm");
+//    form.addEventListener("submit", function(event) {
+//       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+//       alert("submit clicked");
+//    });
+// });
+window.addEventListener('load', () => {
+   console.log("It Loaded!");
+   const pilotName = document.getElementById('pilotName');
+   const copilotName = document.getElementById('copilotName');
+   const fuelLevel = document.getElementById('fuelLevel');
+   const cargoMass = document.getElementById('cargoMass');
+   const form = document.getElementById('form');
+   const errorElement = document.getElementById('launchStatus');
+
+      form.addEventListener('submit', (event) => {
+         let messages = [Junk];
+            if (pilotName.value === '' || pilotName.value === null || typeof(pilotName.value) === String) {
+               messages.push('Pilot Name is required')
+            } 
+            
+            if (copilotName.value === '' || copilotName.value === null || typeof(pilotName.value) === String) {
+               messages.push('Co-Pilot Name is required')
+            } 
+            
+            if (fuelLevel.value === '' || fuelLevel.value === null || typeof(pilotName.value) === Number) {
+               messages.push('Fuel Level is required')
+            } 
+            
+            if (cargoMass.value === '' || cargoMass.value === null || typeof(pilotName.value) === Number) {
+               messages.push('Cargo Mass is required')
+            }
+
+            if (messages.length > 0) {
+            event.preventDefault();
+            errorElement.innerText = messages.join(', ')
+            }
+      });   
+});
+
+
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
